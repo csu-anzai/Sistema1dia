41de27614a4bd2a120fb520cd4d36041a8547acb
Wendell Smith Vidal <wendell18cuenta@gmail.com>
2019-09-03 15:38:17 -0500

    f

--- admin/pages/controlarganancias.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$pventa=$_POST['venta'];
-	$cadena="<input class='form-control' id='lista2' value ='$pventa' name='lista2'>";
-	echo  "<input class='form-control' id='lista2' value ='$pventa' name='lista2'>";
-	
-?>
